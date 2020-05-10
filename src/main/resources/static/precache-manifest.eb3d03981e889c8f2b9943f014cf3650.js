de0873c735dfede2547f38a16e465150ea4166e7
rdx7777 <radek.jerzynski@gmail.com>
2020-05-11 00:53:51 +0100

    added built files into resources/static directory

---
+++ src/main/resources/static/precache-manifest.eb3d03981e889c8f2b9943f014cf3650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fbd8171398ced91a9f9902449475f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10bf7ad723af5d04aca3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "92bfc3a8dd3313a80a37",
+    "url": "/static/css/main.22f1ef59.chunk.css"
+  },
+  {
+    "revision": "10bf7ad723af5d04aca3",
+    "url": "/static/js/2.a24112c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a24112c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92bfc3a8dd3313a80a37",
+    "url": "/static/js/main.61f225c6.chunk.js"
+  },
+  {
+    "revision": "05768a01f7a23096607d",
+    "url": "/static/js/runtime-main.8e63d865.js"
+  }
+]);
