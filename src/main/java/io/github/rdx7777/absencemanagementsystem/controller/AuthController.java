2e932f5c995c7279207714bbe12a3355d1406adc
rdx7777 <radek.jerzynski@gmail.com>
2020-05-15 23:58:10 +0100

    changed CrossOrigin annotation in AuthControler classe

--- src/main/java/io/github/rdx7777/absencemanagementsystem/controller/AuthController.java
+++ src/main/java/io/github/rdx7777/absencemanagementsystem/controller/AuthController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://a-m-system.herokuapp.com/"}, maxAge = 3600)
+@CrossOrigin(origins = {"https://a-m-system.herokuapp.com/", "*"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
