226e0a694227614b13005ba455636621a7e9094c
rdx7777 <radek.jerzynski@gmail.com>
2020-05-16 01:23:18 +0100

    added antMatchers for HttpMethod.OPTIONS in configure method in WebSecurityConfig class

--- src/main/java/io/github/rdx7777/absencemanagementsystem/security/WebSecurityConfig.java
+++ src/main/java/io/github/rdx7777/absencemanagementsystem/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -65,6 +66,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .antMatchers("/api/**").permitAll()
             .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
